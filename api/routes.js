cc796b628feab20fcab39cc199b6756ea746e151
suyuan <suyuan1203@outlook.com>
2014-04-25 13:45:06 +0800

    add service to read allresults

--- api/routes.js
+++ api/routes.js
@@ -1,6 +1,7 @@
 var decisionController = require('./controllers/decision.js');
+var marketshareController = require('./controllers/marketshare.js');
 
 module.exports = function(app){
-    app.get('/', function(req, res, next){res.send("Index");});
     app.get('/api/decision', decisionController.getDecision);
+    app.get('/api/marketshare', marketshareController.getMarketshare);
 }
