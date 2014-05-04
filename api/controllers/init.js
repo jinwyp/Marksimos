c97be77304d055939509c1983a4515ed9c495c58
suyuan <suyuan1203@outlook.com>
2014-05-04 14:35:08 +0800

    save all results text to mongo

--- api/controllers/init.js
+++ api/controllers/init.js
@@ -4,6 +4,7 @@ var config = require('../config.js');
 var Q = require('q');
 var decisionCleaner = require('../convertors/decision.js');
 var decisionModel = require('../models/decision.js');
+var allResultsModel = require('../models/allresults.js');
 
 /**
  * Initialize game data
@@ -14,12 +15,61 @@ var decisionModel = require('../models/decision.js');
 exports.init = function(req, res, next){
     var seminar = req.session.seminar;
 
-    initDecision(seminar).then(function(){
-        res.send('initialize decision success');
+    // initDecision(seminar).then(function(){
+    //     res.send('initialize decision success');
+    // }).fail(function(err){
+    //     next(err);
+    // });
+
+    initAllResult(seminar).then(function(){
+        res.send('Got allresults');
     }).fail(function(err){
         next(err);
-    });
+    }).done();
 };
+
+
+
+function initAllResult(seminar){
+    var periods = config.initPeriods;
+
+    var queries = [];
+    periods.forEach(function(period){
+        queries.push(initOnePeriodAllResult(seminar, period));
+    });
+
+    var p = Q.all(queries)
+    .then(function(results){
+        console.log(results);
+        return allResultsModel.updateAllResults(seminar, results);
+    });
+
+    return p;
+
+    // var allResultList = [];
+
+    // var result = initOnePeriodAllResult(seminar, periods[0]);
+    // periods = periods.slice(1);
+    // periods.forEach(function(period){
+    //     result = result.then(function(input){
+    //         allResultList.push(input);
+    //         return initOnePeriodAllResult(seminar, period);
+    //     })
+    // });
+
+    // result = result.then(function(input){
+    //     return allResultsModel.updateAllResults(seminar, allResultList);
+    // });
+
+    // return result;
+}
+
+function initOnePeriodAllResult(seminar, period){
+    var reqUrl = config.cgiService 
+        + util.format('allresults.exe?seminar=%s&period=%s', seminar, period);
+
+    return request(reqUrl);
+}
 
 /**
  * Initialize decision
@@ -52,7 +102,8 @@ function initDecision(seminar){
  * @return {Object} a promise
  */
 function initOnePeriodDecison(seminar, team, period){
-    var reqUrl = config.cgiService + util.format('decisions.exe?period=%s&team=%s&seminar=%s'
+    var reqUrl = config.cgiService 
+        + util.format('decisions.exe?period=%s&team=%s&seminar=%s'
         , period, team, seminar);
     return request(reqUrl).then(function(result){
         decisionCleaner.clean(result);
--- api/models/allresults.js
+++ api/models/allresults.js
@@ -1,13 +1,24 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var Q = require('q');
 
 var allResultSchema = new Schema({
-    allResult: String
+    seminarId: String,
+    allResult: []
 });
 
-var allResultJson;
+var AllResults = mongoose.model("AllResult", allResultSchema);
 
-exports.updateAllResults = function(allResult){
-    allReslutJson = JSON.parse(allResult);
-    return allResultJson;
+
+exports.updateAllResults = function(seminarId, allResult){
+    var result = new AllResults({seminarId: seminarId, allResult: allResult});
+    result.save(function(err){
+        var deferred = Q.defer();
+        if(err){
+            deferred.reject(err);
+        }else{
+            deferred.resolve(null);
+        }
+        return deferred.promise;
+    });
 };
