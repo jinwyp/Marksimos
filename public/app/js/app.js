3dcf34d64351c08f3d00ca6916d883d55db79919
Jin <jinwyp@gmail.com>
2014-05-09 16:49:18 +0800

    finish report page

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -2,10 +2,76 @@
  * Created by jinwyp on 4/28/14.
  */
 
-var aa ={};
-var aa ={};
-var aa ={};
-var aa ={};
-var aa ={};
-var aa ={};
+// create module for custom directives
+var marksimosapp = angular.module('marksimos', ['angularCharts']);
+
+// controller business logic
+marksimosapp.controller('chartController', function AppCtrl ($scope,  $timeout) {
+    $timeout(function() {
+        $scope.data = {
+            series: ['Sales', 'Income', 'Expense', 'Laptops', 'Keyboards'],
+            data : [{
+                x : "Sales",
+                y: [100,500, 0],
+                tooltip:"this is tooltip"
+            },
+                {
+                    x : "Not Sales",
+                    y: [300, 100, 100]
+                },
+                {
+                    x : "Tax",
+                    y: [351]
+                },
+                {
+                    x : "Not Tax",
+                    y: [54, 0, 879]
+                }]
+        };
+    }, 100);
+
+
+    $scope.data1 = {
+        series: ['Sales', 'Income', 'Expense', 'Laptops', 'Keyboards'],
+        data : [{
+            x : "Sales",
+            y: [100,500, 0],
+            tooltip:"this is tooltip"
+        },
+            {
+                x : "Not Sales",
+                y: [300, 100, 100]
+            },
+            {
+                x : "Tax",
+                y: [351]
+            },
+            {
+                x : "Not Tax",
+                y: [54, 0, 879]
+            }]
+    }
+
+    $scope.chartType = 'bar';
+
+    $scope.config = {
+        labels: false,
+        title : "Not Products",
+        legend : {
+            display:true,
+            position:'left'
+        },
+        innerRadius: 0
+    };
+
+    $scope.config1 = {
+        labels: false,
+        title : "Products",
+        legend : {
+            display:true,
+            position:'right'
+        },
+        lineLegend: 'traditional'
+    }
+});
 
