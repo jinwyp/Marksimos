4165dba73ba5e5f0b08469889730a4576ce7fab0
suyuan <suyuan1203@outlook.com>
2014-05-15 17:14:04 +0800

    add tooltips data to perception map

--- api/convertors/allresults.js
+++ api/convertors/allresults.js
@@ -194,6 +194,7 @@ exports.perceptionMap = function(allResults, exogenous){
             exogenous: []
         };
         
+        //brands data
         for(var j=0; j<periodResult.p_Brands.length; j++){
             var brand = periodResult.p_Brands[j];
             if(company.c_CompanyID === brand.b_ParentCompanyID){
@@ -205,17 +206,20 @@ exports.perceptionMap = function(allResults, exogenous){
             }
         }
 
+        //SKU data
         for(var k=0; k<periodResult.p_SKUs.length; k++){
             var SKU = periodResult.p_SKUs[k];
             if(company.c_CompanyID === SKU.u_ParentCompanyID){
                 companyData.SKUs.push({
                     SKUName: SKU.u_SKUName,
                     imagePerception: SKU.u_Perception[1],
-                    valuePerception: SKU.u_Perception[0]
+                    valuePerception: SKU.u_Perception[0],
+                    tooltips: prepareSKUTooltips(allResults, SKU.u_SKUID)
                 })
             }
         }
 
+        //Exogenous
         var exoSegmentsIdealPoints = exogenous.exo_SegmentsIdealPoints;
         for(var p=0; p<exoSegmentsIdealPoints.length; p++){
             var point = exoSegmentsIdealPoints[p];
@@ -229,6 +233,133 @@ exports.perceptionMap = function(allResults, exogenous){
         result.push(companyData);
     }
     return result;
+}
+
+/**
+ * Prepare tooltips data for SKU label on SKU perception map
+ *
+ * @method prepareSKUToolTips
+ * @param {Object} the JSON object got from allResults CGI
+ * @param {Function} the function to get a certain field of JSON object
+ * @return {Object} chart data
+ */
+function prepareSKUTooltips(allResults, SKUID){
+    if(!allResults){
+        throw new Error("allResults can't be empty.");
+    }
+
+    if(!SKUID){
+        throw new Error("SKUID can't be empty");
+    }
+
+    var currentPeriodResult = allResults[allResults.length-1].onePeriodResult;
+    var perviousPeriodResult = allResults[allResults.length-2].onePeriodResult;
+
+    var tooltips = [];
+
+    var currentPeriodSKU = findSKY(currentPeriodResult, SKUID);
+    var previousPeriodSKU = findSKY(perviousPeriodResult, SKUID);
+
+    //marke share
+    var marketShareChange = compare(currentPeriodSKU.u_ValueSegmentShare[consts.ConsumerSegmentsMax] 
+        , previousPeriodSKU.u_ValueSegmentShare[consts.ConsumerSegmentsMax]);
+    tooltips.push({
+        name: 'Market Share(value %)',
+        value: currentPeriodSKU.u_ValueSegmentShare[consts.ConsumerSegmentsMax],
+        compareWithPreviousPeriod: marketShareChange
+    });
+
+    //average display price
+    var averageDisplayPriceChange = compare(currentPeriodSKU.u_AverageDisplayPrice,
+        previousPeriodSKU.u_AverageDisplayPrice);
+    tooltips.push({
+        name: 'Average display price',
+        value: currentPeriodSKU.u_AverageDisplayPrice,
+        compareWithPreviousPeriod: averageDisplayPriceChange
+    });
+
+    var tIndexChange = compare(getTechnologyLevel(currentPeriodSKU),
+        getTechnologyLevel(previousPeriodSKU));
+    tooltips.push({
+        name: 'Applied Technology Index',
+        value: getTechnologyLevel(currentPeriodSKU),
+        compareWithPreviousPeriod: tIndexChange
+    });
+
+    var qualityChange = compare(getIngredientsQuality(currentPeriodSKU),
+        getIngredientsQuality(previousPeriodSKU));
+    tooltips.push({
+        name: 'Ingredients Quality Index',
+        value: getIngredientsQuality(currentPeriodSKU),
+        compareWithPreviousPeriod: qualityChange
+    });
+
+    tooltips.push({
+        name: 'Awareness(%)',
+        value: currentPeriodSKU.u_Awareness,
+        compareWithPreviousPeriod: compare(currentPeriodSKU.u_Awareness, previousPeriodSKU.u_Awareness)
+    });
+
+    tooltips.push({
+        name: 'Shelf Space(%)',
+        value: currentPeriodSKU.u_ShelfSpace,
+        compareWithPreviousPeriod: compare(currentPeriodSKU.u_ShelfSpace, previousPeriodSKU.u_ShelfSpace)
+    });
+
+    tooltips.push({
+        name: 'Value Perception Change',
+        value: currentPeriodSKU.u_Perception[0],
+        compareWithPreviousPeriod: compare(currentPeriodSKU.u_Perception, previousPeriodSKU.u_Perception[0])
+    });
+
+    tooltips.push({
+        name: 'Image Perception Change',
+        value: currentPeriodSKU.u_Perception[1],
+        compareWithPreviousPeriod: compare(currentPeriodSKU.u_Perception, previousPeriodSKU.u_Perception[1])
+    });
+
+    return tooltips;
+
+    function compare(a, b){
+        var r = a - b;
+        if(r>0){
+            return 1;
+        }else if(r<0){
+            return 0;
+        }else{
+            return -1;
+        }
+    }
+
+    function getTechnologyLevel(SKU){
+        if(SKU.u_ps_RetailStocks[consts.StocksMaxTotal].s_ps_Volume>0){
+            return SKU.u_ps_RetailStocks[consts.StocksMaxTotal].s_Technology;
+
+        }else if(SKU.u_ps_WholesaleStocks[consts.StocksMaxTotal].s_ps_Volume > 0){
+            return SKU.u_ps_WholesaleStocks[consts.StocksMaxTotal].s_Technology;
+
+        }else if(SKU.u_ps_FactoryStocks[consts.StocksMaxTotal].s_ps_Volume > 0){
+            return SKU.u_ps_FactoryStocks[consts.StocksMaxTotal].s_Technology;
+
+        }else{
+            return SKU.u_ps_FactoryStocks[0].s_Technology;
+        }
+    }
+
+    function getIngredientsQuality(SKU){
+        if(SKU.u_ps_RetailStocks[consts.StocksMaxTotal].s_ps_Volume>0){
+            return SKU.u_ps_RetailStocks[consts.StocksMaxTotal].s_IngredientsQuality;
+
+        }else if(SKU.u_ps_WholesaleStocks[consts.StocksMaxTotal].s_ps_Volume > 0){
+            return SKU.u_ps_WholesaleStocks[consts.StocksMaxTotal].s_IngredientsQuality;
+
+        }else if(SKU.u_ps_FactoryStocks[consts.StocksMaxTotal].s_ps_Volume > 0){
+            return SKU.u_ps_FactoryStocks[consts.StocksMaxTotal].s_IngredientsQuality;
+
+        }else{
+            return SKU.u_ps_FactoryStocks[0].s_IngredientsQuality;
+        }
+    }
 }
 
 /**
@@ -326,6 +457,15 @@ function findBrand(currentPeriodResult, u_ParentBrandID){
         var brand = currentPeriodResult.p_Brands[i];
         if(brand.b_BrandID === u_ParentBrandID){
             return brand;
+        }
+    }
+}
+
+function findSKY(periodResult, SKUID){
+    for(var i=0; i<periodResult.p_SKUs.length; i++){
+        var SKU = periodResult.p_SKUs[i];
+        if(SKUID === SKU.u_SKUID){
+            return SKU;
         }
     }
 }
