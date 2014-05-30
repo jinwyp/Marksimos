d93cfec7764d57457c6227c0861b3649cb2c10a1
suyuan <suyuan1203@outlook.com>
2014-05-30 17:23:21 +0800

    change map chart name function

--- api/controllers/chart.js
+++ api/controllers/chart.js
@@ -66,58 +66,12 @@ exports.getChart = function(req, res, next){
 
     function mapChartName(chartName){
         chartName = chartName.toLowerCase();
-        switch(chartName){
-            case 'market_share_in_value':
-                return 'marketShareInValue';
-            case 'market_share_in_volume':
-                return 'marketShareInVolume';
-            case 'mind_space_share':
-                return 'mindSpaceShare';
-            case 'shelf_space_share':
-                return 'shelfSpaceShare';
-            case 'total_investment':
-                return 'totalInvestment';
-            case 'net_profit_by_companies':
-                return 'netProfitByCompanies';
-            case 'return_on_investment':
-                return 'returnOnInvestment';
-            case 'investments_versus_budget':
-                return 'investmentsVersusBudget';
-            case 'market_sales_value':
-                return 'marketSalesValue';
-            case 'market_sales_volume':
-                return 'marketSalesVolume';
-            case 'total_inventory_at_factory':
-                return 'totalInventoryAtFactory';
-            case 'total_inventory_at_trade':
-                return 'totalInventoryAtTrade';
-            case 'segments_leaders_by_value_price_sensitive':
-                return 'segmentsLeadersByValuePriceSensitive';
-            case 'segments_leaders_by_value_pretenders':
-                return 'segmentsLeadersByValuePretenders';
-            case 'segments_leaders_by_value_moderate':
-                return 'segmentsLeadersByValueModerate';
-            case 'segments_leaders_by_value_good_life':
-                return 'segmentsLeadersByValueGoodLife';
-            case 'segments_leaders_by_value_ultimate':
-                return 'segmentsLeadersByValueUltimate';
-            case 'segments_leaders_by_value_pragmatic':
-                return 'segmentsLeadersByValuePragmatic';
-            case 'growth_rate_in_volume':
-                return 'growthRateInVolume';
-            case 'growth_rate_in_value':
-                return 'growthRateInValue';
-            case 'net_market_price':
-                return 'netMarketPrice';
-            case 'segment_value_share_total_market':
-                return 'segmentValueShareTotalMarket';
-            case 'perception_map':
-                return 'perceptionMap';
-            case 'inventory_report':
-                return 'inventoryReport';
-            default:
-                return '';
+        var names = chartName.split('_');
+        for(var i=1; i<names.length; i++){
+            names[i] = names[i][0].toUpperCase() + names[i].substring(1);
         }
+
+        return names.join('');
     }
 };
 
