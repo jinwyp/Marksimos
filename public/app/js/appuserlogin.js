72ff5b6578be02abc25570055bc5842098a8073c
ludwik.applechina@gmail.com <ludwik.applechina@gmail.com>
2014-06-05 13:06:27 +0800

    update readme

--- public/app/js/appuserlogin.js
+++ public/app/js/appuserlogin.js
@@ -16,8 +16,6 @@ marksimosapp.factory('currentUser',function(){
         currentRound : -3, // -3,-2, -1, 0, 1, 2, 3, 4, 5, 6
         companylist : []
     };
-
-
     return {
 
     }
@@ -27,7 +25,6 @@ marksimosapp.factory('currentUser',function(){
 
 // controller business logic
 marksimosapp.controller('userLoginController', function AppCtrl ($scope,  $timeout, $http, $window) {
-
     $scope.css = {
         newUser : {
             passwordPrompt : false
@@ -42,9 +39,8 @@ marksimosapp.controller('userLoginController', function AppCtrl ($scope,  $timeo
     };
 
     $scope.login = function(form){
-
         if(form.$valid){
-            $http.post('/api/login', $scope.data.newUser).success(function(data, status, headers, config){
+             $http.post('/api/login', $scope.data.newUser).success(function(data, status, headers, config){
                 if(data.status == 303){
                     $window.location.href = "/introduction" ;
                 }else if(data.status == 401){
@@ -54,13 +50,11 @@ marksimosapp.controller('userLoginController', function AppCtrl ($scope,  $timeo
                     form.password.$invalid = true;
                     $scope.css.newUser.passwordPrompt = true;
                 }
-
             }).error(function(data, status, headers, config){
 
             });
         }
     };
-
 
 });
 
