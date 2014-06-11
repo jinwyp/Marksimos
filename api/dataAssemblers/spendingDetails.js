0e10d32eec7db4b5146520b9c7e1c859cf9e79d7
suyuan <suyuan1203@outlook.com>
2014-06-11 11:08:19 +0800

    add more data to spending detail

--- api/dataAssemblers/spendingDetails.js
+++ api/dataAssemblers/spendingDetails.js
@@ -2,6 +2,7 @@ var decisionAssembler = require('../dataAssemblers/decision.js');
 var seminarModel = require('../models/seminar.js');
 var Q = require('q');
 var utility = require('../utility.js');
+var consts = require('../consts.js');
 
 exports.getSpendingDetails = function(seminarId, period, companyId){
     return Q.all([
@@ -12,6 +13,7 @@ exports.getSpendingDetails = function(seminarId, period, companyId){
         var brandData = [];
         var companyData = {}
 
+        //assemble brand data
         for(var i=0; i<decision.d_BrandsDecisions.length; i++){
             var brandDecision = decision.d_BrandsDecisions[i];
             var brandDataRow = {};
@@ -61,18 +63,71 @@ exports.getSpendingDetails = function(seminarId, period, companyId){
         }, 0)
         brandData.push(total);
 
+        //assemble company data
+        companyData.investmentInProductionEfficiency = decision.d_InvestmentInEfficiency;
+        companyData.investmentInProcessingTechnology = decision.d_InvestmentInTechnology;
+
+        companyData.totalInvestment = (total.salesForce + total.consumerCommunication + total.consumerPromotion
+        + total.tradeExpenses + total.estimatedAdditionalTradeMarginCost + total.estimatedWholesaleBonusCost).toFixed(2);
 
         var companyDataInAllResults = utility.findCompany(seminar.allResults[seminar.allResults.length-1], companyId)
         //average budget per period
         companyData.averageBudgetPerPeriod = (companyDataInAllResults.c_TotalInvestmentBudget / seminar.simulationSpan).toFixed(2);
 
-        companyData.investmentInProductionEfficiency = decision.d_InvestmentInEfficiency;
-        companyData.investmentInProcessingTechnology = decision.d_InvestmentInTechnology;
+        companyData.totalInvestmentBudget = companyDataInAllResults.c_TotalInvestmentBudget;
 
+        companyData.cumulatedPreviousInvestments = companyDataInAllResults.c_CumulatedInvestments.toFixed(2);
 
+        companyData.availableBudget = (companyDataInAllResults.c_TotalInvestmentBudget - companyDataInAllResults.c_CumulatedInvestments 
+            - companyData.totalInvestment).toFixed(2);
+
+        companyData.normalCapacity = calculateTotalVolume(decision);
+
+        companyData.availableOvertimeCapacityExtension = '';
+
+        companyData.acquiredEfficiency = '';
+
+        companyData.acquiredProductionVolumeFlexibility = '';
+
+        companyData.acquiredTechnologyLevel = '';
+
+        
         return {
             brandData: brandData,
             companyData: companyData
         };
     });
 }
+
+function calculateTotalVolume(currentDecision){
+    var totalVolume = 0;
+    currentDecision.d_BrandsDecisions.forEach(function(brandDecision){
+        brandDecision.d_SKUsDecisions.forEach(function(SKUDecision){
+            totalVolume += SKUDecision.d_ProductionVolume * consts.ActualSize[SKUDecision.d_PackSize];
+        })
+    })
+    return totalVolume;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
