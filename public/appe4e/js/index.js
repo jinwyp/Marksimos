062921d189b5b143baaca6075f5a4c0aa1fed210
raven <monsoul@msn.com>
2014-09-24 12:15:06 +0800

    update

--- public/appe4e/js/index.js
+++ public/appe4e/js/index.js
@@ -3,11 +3,16 @@ $(function(){
 		navigation: true,
 		navigationPosition: 'right',
 		anchors: ['firstPage', 'secondPage', '3rdPage'],
-		sectionsColor: ['#fff', '#1BBC9B', '#7E8F7C'],
+		sectionsColor: ['#fff', '#FF441C', '#fff'],
+		navigationColor:'#B3B3B3',
 		css3: true,
 		scrollingSpeed: 1700,
 		easing: 'easeOutBack',
 		resize : false,
-		scrollOverflow: true
+		scrollOverflow: true,
+		loopBottom:true,
+		slidesNavigation:true,
+		slidesNavPosition:'top',
+		slidesNavigationTitle:["What's hcd learning","what can you expct"]
 	});
 });
