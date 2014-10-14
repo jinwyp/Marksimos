b0851bde5d5dcc0cdde0e6ac8470230b3c3ed830
Jin <jinwyp@gmail.com>
2014-10-14 18:30:10 +0800

    finish css sprite using sass

--- public/app/css/config.rb
+++ public/app/css/config.rb
@@ -5,13 +5,16 @@ http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
+generated_images_dir = "images"
+
 javascripts_dir = "../js"
+
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
