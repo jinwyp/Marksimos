e64aaf22043c4da937f2d9afb69be0b5e246cf2a
ludwik.applechina@gmail.com <ludwik.applechina@gmail.com>
2014-11-24 17:01:02 +0800

    test

--- common/config.js
+++ common/config.js
@@ -24,7 +24,7 @@ module.exports = (function(){
         },
 
         segmentNameAndIndex: {
-            'priceSensitive':0, 
+            'priceSensitive':0,
             'pretenders': 1,
             'moderate': 2,
             'goodLife': 3,
@@ -67,9 +67,9 @@ module.exports = (function(){
         },
 
         packsizeDescription: [
-            "Small Pack", 
-            "Normal Pack", 
-            "Large Pack" 
+            "Small Pack",
+            "Normal Pack",
+            "Large Pack"
         ]
     };
 
@@ -94,10 +94,12 @@ module.exports = (function(){
             break;
         case 'ludwik':
             config.logDirectory = '/Users/ludwik/code/actived/Marksimos/log/';
-            config.mongo_conn = 'mongodb://localhost/Marksimos';
+            config.mongo_conn = 'mongodb://192.168.2.50/Marksimos';
             config.host = 'http://localhost:3000/';
-            config.cgiService = 'http://localhost:8080/cgi-bin/marksimos/';            
-            break;            
+            config.cgiService = 'http://192.168.2.50/cgi-bin/';
+            //config.mongo_conn = 'mongodb://localhost/Marksimos';
+            //config.cgiService = 'http://localhost:8080/cgi-bin/marksimos/';
+            break;
         case 'development':
             config.logDirectory = '/Users/ludwik/code/Marksimos/log/';
             config.mongo_conn = 'mongodb://localhost/Marksimos';
