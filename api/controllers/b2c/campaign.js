c22dbb2066d8c4e20350079716fb7135412e248e
Jin <jinwyp@gmail.com>
2015-03-21 13:08:52 +0800

    add

--- api/controllers/b2c/campaign.js
+++ api/controllers/b2c/campaign.js
@@ -41,7 +41,7 @@ exports.campaignSingleInfoPage = function(req, res, next){
         return res.status(400).send( {message: validationErrors} );
     }
 
-    campaignModel.findOne({_id: req.params.campaignId, activated: true}).populate('seminarListMarksimos').populate('teamList').execQ().then(function(resultCampaign){
+    campaignModel.findOne({_id: req.params.campaignId, activated: true}).populate('seminarListMarksimos').populate('teamList').populate('pictures.listCover').populate('pictures.firstCover').populate('pictures.benefit1').populate('pictures.benefit2').populate('pictures.benefit3').populate('pictures.qualification').execQ().then(function(resultCampaign){
         if(!resultCampaign){
             return res.status(400).send( {message: "campaign doesn't exist."});
         }
