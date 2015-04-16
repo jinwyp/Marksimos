7f861bac17634f2d987c194c440a990b2e2c978e
Jin <jinwyp@gmail.com>
2015-04-16 17:59:28 +0800

    add close for mutiselect

--- public/app/js/websitecomponent.js
+++ public/app/js/websitecomponent.js
@@ -187,6 +187,7 @@
                 show        : '=',
                 datasource  : '=',
                 selectclick : '&',
+                close : '&',
                 ngModel : '=',
                 selectfitler : '@'
             },
