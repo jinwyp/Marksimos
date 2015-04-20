954d1d40055a943a3155108f42de22ec825006cc
Jin <jinwyp@gmail.com>
2015-04-20 12:27:19 +0800

    add model chat message

---
+++ api/models/b2c/chatmessage.js
@@ -0,0 +1,82 @@
+/**
+ * Created by jinwyp on 4/20/15.
+ */
+
+
+/**
+ * Created by jinwyp on 3/3/15.
+ */
+
+'use strict';
+
+/*!
+ * Module dependencies
+ */
+var mongoose = require('mongoose-q')(require('mongoose'), {spread:true});
+var Schema = mongoose.Schema;
+var schemaObjectId = Schema.Types.ObjectId;
+var Q = require('q');
+var mongooseTimestamps = require('mongoose-timestamp');
+
+
+/**
+ * Mongoose schema
+ */
+var chatMessageSchema = new Schema({
+
+    text: { type: String },
+
+    creator: { type: schemaObjectId, ref: 'User' },
+
+    room : {
+        roomNumber : {type: String},
+        description: { type: String }
+    }
+
+});
+
+/**
+ * Mongoose plugin
+ */
+chatMessageSchema.plugin(mongooseTimestamps);
+
+
+/**
+ * Add your
+ * - pre-save hooks
+ * - validations
+ * - virtuals
+ */
+
+
+
+/**
+ * Statics
+ */
+
+chatMessageSchema.statics.updateValidations = function(req){
+
+    //req.sanitize('activated').toBoolean();
+
+    //req.checkBody('name', 'Campaign name should be 2-50 characters').notEmpty().len(2, 50);
+    //req.checkBody('description', 'Campaign description should be 2-10000 characters').notEmpty().len(2, 10000);
+
+    //req.checkBody('activated', 'Campaign activated should Boolean true or false').notEmpty();
+
+    return req.validationErrors();
+};
+
+
+/**
+ * Methods
+ */
+
+
+
+/**
+ * Register Model
+ */
+
+
+var Campaign = mongoose.model("Chatmessage", chatMessageSchema);
+module.exports = Campaign;
