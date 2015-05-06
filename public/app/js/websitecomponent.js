912e66a72762d66b04ea5feafe38eb0af7de3aa4
Jin <jinwyp@gmail.com>
2015-05-06 12:27:00 +0800

    add read

--- public/app/js/websitecomponent.js
+++ public/app/js/websitecomponent.js
@@ -28,83 +28,6 @@
 
     angular.module('marksimos.websitecomponent').directive('chatWindow', ['$q', chatWindowComponent]);
 
-    function chatWindowComponent($q) {
-        return {
-            restrict: 'E',
-            scope: {
-                me: '=username',
-                seminarMessages: '=',
-                companyMessages: '=',
-                dictionaryMessages: '=',
-                sendSeminarMessage: '&',
-                sendCompanyMessage: '&',
-                sendDictionaryMessage: '&',
-                hideChatHeader: '='
-            },
-            templateUrl: 'chatwindow.html',
-            link: function(scope, elem, attrs, ctrl) {
-                scope.data = {
-                    seminarInput: null,
-                    companyInput: null,
-                    dictionaryInput: null
-                };
-                scope.css = {
-                    currentChatTab: 'seminar',
-                    showChat: false,
-                    newMessage: false,
-                    currentTab: 'chat'
-                };
-
-                var chatWindow = elem[0];
-                chatWindow.addEventListener('keydown', function(event) {
-                    // todo: handle line break?
-
-                    if (event.keyCode != 13 || event.target.tagName.toUpperCase() != 'TEXTAREA' || (!scope.data.seminarInput && !scope.data.companyInput && !scope.data.dictionaryInput) ) return;
-
-                    var target = angular.element(event.target);
-                    //var matches = target.matches || target.msMatchesSelector;
-                    //matches = matches.bind(target);
-
-                    if (target.hasClass('seminar')) {
-                        $q.when(scope.sendSeminarMessage({messageInput: scope.data.seminarInput})).then(function() {
-                            console.log(scope.data.seminarInput);
-                            scope.data.seminarInput = '';
-                        });
-                    } else if(target.hasClass('company')) {
-                        $q.when(scope.sendCompanyMessage({messageInput: scope.data.companyInput})).then(function() {
-                            scope.data.companyInput = '';
-                        });
-                    } else if(target.hasClass('dictionary')) {
-                        $q.when(scope.sendDictionaryMessage({messageInput: scope.data.dictionaryInput})).then(function() {
-                            scope.data.dictionaryInput = '';
-                        });
-                    }
-                });
-
-                scope.$watchCollection('seminarMessages', scrollToBottom);
-                scope.$watchCollection('companyMessages', scrollToBottom);
-                scope.$watchCollection('dictionaryMessages', scrollToBottom);
-
-                function scrollToBottom() {
-                    if (scope.seminarMessages.length || (scope.companyMessages && scope.companyMessages.length)) {
-                        scope.css.newMessage = true;
-                    }
-
-                    scope.$$postDigest(function() {
-                        if (!scope.css.showChat) return;
-                        var messagesWindow = chatWindow.querySelector('.messages');
-                        messagesWindow.scrollTop = messagesWindow.scrollHeight;
-                    });
-                }
-
-                scope.clickToggleWindow = function(tab) {
-                    if (tab) scope.css.currentTab = tab;
-                    scope.css.showChat = !scope.css.showChat;
-                    scope.css.newMessage = false;
-                };
-            }
-        };
-    }
 
 
 
@@ -346,6 +269,108 @@
             }
         };
     }
+
+
+
+    function chatWindowComponent($q) {
+        return {
+            restrict: 'E',
+            scope: {
+                me: '=username',
+                seminarMessages: '=',
+                companyMessages: '=',
+                dictionaryMessages: '=',
+                sendSeminarMessage: '&',
+                sendCompanyMessage: '&',
+                sendDictionaryMessage: '&',
+                hideChatHeader: '='
+            },
+            templateUrl: 'chatwindow.html',
+            link: function(scope, elem, attrs, ctrl) {
+                scope.data = {
+                    seminarInput: null,
+                    companyInput: null,
+                    dictionaryInput: null
+                };
+                scope.css = {
+                    currentChatTab: 'seminar',
+                    showChat: false,
+                    newMessage: false,
+                    currentTab: 'chat'
+                };
+
+                var chatWindow = elem[0];
+                chatWindow.addEventListener('keydown', function(event) {
+                    // todo: handle line break?
+
+                    if (event.keyCode != 13 || event.target.tagName.toUpperCase() != 'TEXTAREA' || (!scope.data.seminarInput && !scope.data.companyInput && !scope.data.dictionaryInput) ) return;
+
+                    var target = angular.element(event.target);
+                    //var matches = target.matches || target.msMatchesSelector;
+                    //matches = matches.bind(target);
+
+                    if (target.hasClass('seminar')) {
+                        $q.when(scope.sendSeminarMessage({messageInput: scope.data.seminarInput})).then(function() {
+                            console.log(scope.data.seminarInput);
+                            scope.data.seminarInput = '';
+                        });
+                    } else if(target.hasClass('company')) {
+                        $q.when(scope.sendCompanyMessage({messageInput: scope.data.companyInput})).then(function() {
+                            scope.data.companyInput = '';
+                        });
+                    } else if(target.hasClass('dictionary')) {
+                        $q.when(scope.sendDictionaryMessage({messageInput: scope.data.dictionaryInput})).then(function() {
+                            scope.data.dictionaryInput = '';
+                        });
+                    }
+                });
+
+                scope.$watchCollection('seminarMessages', scrollToBottom);
+                scope.$watchCollection('companyMessages', scrollToBottom);
+                scope.$watchCollection('dictionaryMessages', scrollToBottom);
+
+                function scrollToBottom() {
+                    if (scope.seminarMessages.length || (scope.companyMessages && scope.companyMessages.length)) {
+                        scope.css.newMessage = true;
+                    }
+
+                    scope.$$postDigest(function() {
+                        if (!scope.css.showChat) return;
+                        var messagesWindow = chatWindow.querySelector('.messages');
+                        messagesWindow.scrollTop = messagesWindow.scrollHeight;
+                    });
+                }
+
+                scope.clickToggleWindow = function(tab) {
+                    if (tab) scope.css.currentTab = tab;
+                    scope.css.showChat = !scope.css.showChat;
+                    scope.css.newMessage = false;
+                };
+            }
+        };
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     angular.module('marksimos.websitecomponent').directive('tableReportCompanyStatusSku', function() {
@@ -659,6 +684,7 @@
             };
         }
     ]);
+
 
     angular.module('marksimos.websitecomponent').directive('highlightKey', function () {        
         return {
