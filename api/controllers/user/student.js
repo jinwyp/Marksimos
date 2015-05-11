a6bf4f7bbfab04b1662f6c06c41fe9c597a15ebd
bpceee <bipengcheng@gmail.com>
2015-05-11 15:15:03 +0800

    check mobile phone in profile update only when mobilePhone field uploaded

--- api/controllers/user/student.js
+++ api/controllers/user/student.js
@@ -27,15 +27,11 @@ exports.updateStudentB2CInfo = function(req, res, next){
 
     var updatedUser = _.pick(req.body, pickedUpdatedKeys);
 
+    var promise = Q();
+
     if(updatedUser.mobilePhone && req.user.mobilePhone !== updatedUser.mobilePhone) {
         updatedUser.phoneVerified = false;
-    }
-
-    _.extend(req.user, updatedUser);
-
-    var promise = Q();
-    if(req.body.mobilePhone){
-        promise = userModel.findOneQ({ mobilePhone : req.body.mobilePhone})
+        promise = userModel.findOneQ({mobilePhone : req.body.mobilePhone})
         .then(function(resultUser) {
             if (resultUser) {
                 throw new MKError('Cancel promise chains. Because mobilePhone is existed.', MKError.errorCode.userInfo.phoneExisted);
@@ -44,10 +40,10 @@ exports.updateStudentB2CInfo = function(req, res, next){
     }
 
     promise.then(function(){
+        _.extend(req.user, updatedUser);
         return req.user.saveQ();
     })
     .then(function(savedDoc) {
-        console.log(savedDoc);
         if(savedDoc[1] === 0 ){
             throw new MKError('Cancel promise chains. Because Update User failed. no record is updated. !', MKError.errorCode.common.notUpdate);
         }
