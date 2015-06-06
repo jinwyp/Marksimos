1dda67037cfc8b1e2d054bde1ae8ac0dc70642aa
mountainmoon <yuekec@gmail.com>
2015-06-06 21:11:43 +0800

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -261,6 +261,19 @@ gulp.task('nodemonyuekecheng', function () {
 //        .on('restart', 'default')
 });
 
+gulp.task('nodemonykchome', function () {
+  nodemon({
+    script: 'app.js',
+    ignore : [
+      "tmp/**",
+      "public/**",
+      "views/**"
+    ],
+    env: { 'NODE_ENV': 'ykchome'}
+  });
+//        .on('restart', 'default')
+});
+
 
 
 
@@ -362,6 +375,7 @@ gulp.task('jin', [ 'compass', 'templates', 'nodemonjin', 'watchdev']);
 gulp.task('jinco', ['compass', 'templates', 'nodemonjinlocal', 'watchdev']);
 gulp.task('jinpro', ['compass', 'templates', 'minifycssMarksimos', 'minifycssB2C', 'jscompress', 'nodemonjin', 'watch']);
 gulp.task('yuekecheng', ['compass', 'templates', 'nodemonyuekecheng', 'watchdev']);
+gulp.task('ykchome', ['compass', 'templates', 'nodemonykchome', 'watchdev']);
 gulp.task('minify', ['compass', 'templates', 'minifycssMarksimos', 'minifycssB2C', 'jscompress']);
 
 
